f00a5c30144ddd79566a9b0991cd9be12b535c93
Ryan Reis Poltronieri <ryanreisunivap@gmail.com>
2026-03-22 22:09:31 -0300

    Delete lab3/projeto1/Projeto1 directory

--- lab3/projeto1/Projeto1/src/main/java/org/example/projeto1/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.projeto1.repository;
-
-import org.example.projeto1.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
